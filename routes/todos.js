1212f4aaf8da7e972a69c58acbbb761283c7587b
AyushSingh16 <ayushsingh888777@gmail.com>
2024-06-25 16:26:32 +0530

    Updated todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
-
 //importing controller
-const {createTodo } = require("../controllers/createTodo");
-
+const {createTodo} = require("../controllers/createTodo");
+const {getTodo,getTodoById} = require("../controllers/getTodo");
+const {updateTodo} = require("../controllers/updateTodo");
+const {deleteTodo} = require("../controllers/deleteTodo");
 
 //defining API routes
 
 router.post("/createTodo",createTodo);  //mapped path to controller
+
+router.get("/getTodos",getTodo);
+
+router.get("/getTodos/:id",getTodoById);
+
+router.put("/updateTodo/:id",updateTodo);
+
+router.delete("/deleteTodo/:id",deleteTodo);
 
 module.exports = router;
